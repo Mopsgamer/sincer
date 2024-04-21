22d31b9b92730bed09a230c2e4b781a00f56c0ac
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-04-21 18:43:38 +0200

    cli move messages to variable

--- bin/sincer.js
+++ bin/sincer.js
@@ -19,61 +19,68 @@ function wrap(promise) {
 		})
 }
 
+const description = {
+	namestr : 'string. glob pattern characters available',
+	integer : 'integer',
+	glob:  'glob pattern string. see https://www.npmjs.com/package/picomatch#globbing-features',
+	date: 'date time string. example: \'04-13-2024 10:30:00\'. see https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format'
+}
+
 program.command('list')
 	.aliases(['ls'])
-	.argument('[name]', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
+	.argument('[name]', description.glob)
 	.description('get list of records')
 	.action(function (name) {
 		wrap(sincer.showAll(name))
 	})
 program.command('add')
 	.aliases(['new', 'create'])
-	.argument('[name]', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
+	.argument('[name]', description.glob)
 	.description('create record')
-	.option('--date [date]')
+	.option('--date [date]', description.date)
 	.option('--below')
 	.action(function (name, options) {
 		wrap(sincer.add(name, options))
 	})
 program.command('redate')
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
-	.argument('[newdate]', 'date time string. example: \'04-13-2024 10:30:00\'. see https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format')
+	.argument('<name>', description.glob)
+	.argument('[newdate]', description.date)
 	.description('set record date')
 	.action(function (name, newdate) {
 		wrap(sincer.redate(name, newdate))
 	})
 program.command('rename')
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
-	.argument('[newname]', 'string. glob pattern characters available')
+	.argument('<name>', description.glob)
+	.argument('[newname]', description.namestr)
 	.description('set record name')
 	.action(function (name, newname) {
 		wrap(sincer.rename(name, newname))
 	})
 program.command('swap')
 	.addArgument(new Argument('<mode>', 'swap mode').choices(['places', 'names']))
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
-	.argument('<name2>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
+	.argument('<name>', description.glob)
+	.argument('<name2>', description.glob)
 	.description('swap two records')
 	.action(function (mode, name, name2) {
 		wrap(sincer.swap(name, name2, mode))
 	})
 program.command('up')
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
-	.argument('<count>', 'integer')
+	.argument('<name>', description.glob)
+	.argument('<count>', description.integer)
 	.description('move record up')
 	.action(function (name, count) {
 		wrap(sincer.moveUp(name, count))
 	})
 program.command('down')
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
-	.argument('<count>', 'integer')
+	.argument('<name>', description.glob)
+	.argument('<count>', description.integer)
 	.description('move record down')
 	.action(function (name, count) {
 		wrap(sincer.moveDown(name, count))
 	})
 program.command('remove')
 	.aliases(['rm'])
-	.argument('<name>', 'glob pattern. see https://www.npmjs.com/package/picomatch#globbing-features')
+	.argument('<name>', description.glob)
 	.description('delete records')
 	.action(function (name) {
 		wrap(sincer.remove(name))
