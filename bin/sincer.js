30f8fae01f26c9f103dd5852bf37a46da98b2a5f
Mopsgamer <79159094+Mopsgamer@users.noreply.github.com>
2024-04-21 20:06:30 +0200

    improve help

--- bin/sincer.js
+++ bin/sincer.js
@@ -20,59 +20,60 @@ function wrap(promise) {
 }
 
 const description = {
-	namestr : 'string. glob pattern characters available',
+	namestr : 'string',
 	integer : 'integer',
-	glob:  'glob pattern string. see https://www.npmjs.com/package/picomatch#globbing-features',
+	globFirst:  'glob pattern string. the first record on top. see https://www.npmjs.com/package/picomatch#globbing-features',
+	globSet:  'glob pattern string. set of records. see https://www.npmjs.com/package/picomatch#globbing-features',
 	date: 'date time string. example: \'04-13-2024 10:30:00\'. see https://tc39.es/ecma262/multipage/numbers-and-dates.html#sec-date-time-string-format'
 }
 
 program.command('list')
 	.aliases(['ls'])
-	.argument('[name]', description.glob)
-	.description('get list of records')
+	.argument('[name]', description.globSet)
+	.description('print set of records')
 	.action(function (name) {
 		wrap(sincer.showAll(name))
 	})
 program.command('add')
 	.aliases(['new', 'create'])
-	.argument('[name]', description.glob)
-	.description('create record')
+	.argument('[name]', description.namestr)
+	.description('create new record')
 	.option('--date [date]', description.date)
-	.option('--below')
+	.option('--below', 'add once from below, not from above')
 	.action(function (name, options) {
 		wrap(sincer.add(name, options))
 	})
 program.command('redate')
-	.argument('<name>', description.glob)
+	.argument('<name>', description.globFirst)
 	.argument('[newdate]', description.date)
-	.description('set record date')
+	.description('change date for single record')
 	.action(function (name, newdate) {
 		wrap(sincer.redate(name, newdate))
 	})
 program.command('rename')
-	.argument('<name>', description.glob)
+	.argument('<name>', description.globFirst)
 	.argument('[newname]', description.namestr)
-	.description('set record name')
+	.description('rename single record')
 	.action(function (name, newname) {
 		wrap(sincer.rename(name, newname))
 	})
 program.command('swap')
 	.addArgument(new Argument('<mode>', 'swap mode').choices(['places', 'names']))
-	.argument('<name>', description.glob)
-	.argument('<name2>', description.glob)
-	.description('swap two records')
+	.argument('<name>', description.globFirst)
+	.argument('<name2>', description.globFirst)
+	.description('swap two records (names or places)')
 	.action(function (mode, name, name2) {
 		wrap(sincer.swap(name, name2, mode))
 	})
 program.command('up')
-	.argument('<name>', description.glob)
+	.argument('<name>', description.globFirst)
 	.argument('<count>', description.integer)
 	.description('move record up')
 	.action(function (name, count) {
 		wrap(sincer.moveUp(name, count))
 	})
 program.command('down')
-	.argument('<name>', description.glob)
+	.argument('<name>', description.globFirst)
 	.argument('<count>', description.integer)
 	.description('move record down')
 	.action(function (name, count) {
@@ -80,8 +81,8 @@ program.command('down')
 	})
 program.command('remove')
 	.aliases(['rm'])
-	.argument('<name>', description.glob)
-	.description('delete records')
+	.argument('<name>', description.globSet)
+	.description('delete a couple of records')
 	.action(function (name) {
 		wrap(sincer.remove(name))
 	})
