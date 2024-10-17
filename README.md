b2a5ad6aa2b82a636f7ecc30864e05bf1eb427e8
Mopsgamer <kisillev020304@gmail.com>
2024-10-17 12:06:55 +0200

    Revert "move to deno"
    This reverts commit 2f288ffa2e1ecc27a925febefe6bb42b2a80a6c7.

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # sincer
-
-Node.js terminal client for time monitoring. Inspired by
-[TimeSince](https://play.google.com/store/apps/details?id=es.desaway.timesince).
+Node.js terminal client for time monitoring. Inspired by [TimeSince](https://play.google.com/store/apps/details?id=es.desaway.timesince).
 
 Only for me.
---
+++ test/methods.test.js
@@ -0,0 +1,11 @@
+import * as chai from 'chai'
+import {normalConfig} from '../lib/index.js'
+
+describe('Methods', function () {
+	it('normalConfig', function () {
+		const since = new Date().toJSON()
+		const input = {records: [{name: 'test', since: since, abcd: '0123'}], count: 0}
+		const output = normalConfig(input)
+		chai.assert.deepEqual(output, {records: [{name: 'test', since: since}], count: 0})
+	})
+})
